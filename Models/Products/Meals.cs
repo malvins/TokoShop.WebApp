854c4ba491070cad9da42c0da33f672aa9f29c10
M Alvin Syahrin <alvin.syahrin0795@gmail.com>
2023-11-01 22:11:53 +0700

    product list

---
+++ Models/Products/Meals.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TokoShop.WebApp.Models.Products
+{
+    public class Meal
+    {
+        [JsonPropertyName("strMeal")]
+        public string StrMeal { get; set; }
+
+        [JsonPropertyName("strMealThumb")]
+        public string StrMealThumb { get; set; }
+
+        [JsonPropertyName("idMeal")]
+        public string IdMeal { get; set; }
+    }
+
+    public class MealRoot
+    {
+        [JsonPropertyName("meals")]
+        public List<Meal> Meals { get; set; }
+    }
+}
